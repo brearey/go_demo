b618a05bdfc1235b58b914a6eb917c57714d4291
Евгений <brearey4@gmail.com>
2025-10-29 23:00:07 +0900

    int array

--- main.go
+++ main.go
@@ -1,12 +1,13 @@
 package main
 
-import (
-	"fmt"
-	"os"
-)
+import "fmt"
 
 func main() {
-	for i := 1; i < len(os.Args); i++ {
-		fmt.Printf("%d) arg = %v\n", i, os.Args[i])
+	// for i := 1; i < len(os.Args); i++ {
+	// 	fmt.Printf("%d) arg = %v\n", i, os.Args[i])
+	// }
+	var arr [3]int = [3]int{1, 2, 3}
+	for idx, value := range arr {
+		fmt.Printf("%d) %d\n", idx, value)
 	}
 }
