3859605f8f0a01ea1bdf5710c01bb469e4911507
lorriant <brearey4@gmail.com>
2025-11-16 18:27:03 +0900

    test pass begin

---
+++ password_test.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestGeneratePassword(t *testing.T) {
+	const passLength = 10
+	var password, err = Generate(passLength)
+
+	if (err != nil) {
+		t.Error("функция Generate не должна вернуть ошибку")
+	}
+
+	if (len(password) != passLength) {
+		t.Errorf("длина пароля ожидалась %d, но получили %d", passLength, len(password))
+	}
+
+	if (reflect.TypeOf(password).Kind() != reflect.String) {
+		t.Errorf("ожидалась строка но получили %v", reflect.TypeOf(password))
+	}
+}
